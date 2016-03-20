66832723077002b1126074670a3a52a3ffc37014
Lukino <lukino@debian-development>
2016-03-20 01:34:23 +0100

    Add ngrx/store

--- src/vendor.ts
+++ src/vendor.ts
@@ -9,9 +9,9 @@ import 'angular2/core';
 import 'angular2/http';
 import 'angular2/router';
 
-// RxJS
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';
+// Extra
+import 'rxjs';
+import '@ngrx/store';
 
 if ('production' === ENV) {
   // Production
